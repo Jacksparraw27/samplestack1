dc26f12842f31a9d301008870db2fa1d07e84aeb
Saiful  Islam <saifulislam270503@gmail.com>
2022-12-01 16:32:23 +0700

    Add project files.

---
+++ samplestack1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StackUsingLinkedList_CSharp
+{
+    class Node
+    {
+        public int info;
+        public Node Node;
+        public Node(int i, Node n)
+        {
+            info = 1;
+            next = n;
+        }
+    }
+}
